a42c2c09db3b8a2bbd9be82177b572f1b35d02b8
ninon <ninon@nout.fr>
2014-11-13 10:16:57 +0100

    generation du bundle

---
+++ Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace NOUT\Bundle\ContextesBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('NOUTContextesBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
---
+++ NOUTContextesBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace NOUT\Bundle\ContextesBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class NOUTContextesBundle extends Bundle
+{
+}
