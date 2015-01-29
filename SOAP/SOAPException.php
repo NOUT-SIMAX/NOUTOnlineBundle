c47f53875e073abe8717f1697a36d32a91c1669b
ninon <ninon@nout.fr>
2015-01-29 10:23:37 +0100

    suppression classe qui sert pas conservation du message d'origine

--- SOAP/SOAPException.php
+++ SOAP/SOAPException.php
@@ -10,8 +10,22 @@ namespace NOUT\Bundle\NOUTOnlineBundle\SOAP;
 
 class SOAPException extends \Exception
 {
+	/**
+	 * @var string
+	 */
+	protected $messageOrigine;
+
 	public function __construct($message = "", $code = 0, Exception $previous = null)
 	{
+		$this->messageOrigine = $message;
 		parent::__construct($code.' '.$message, $code, $previous);
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getMessageOrigine()
+	{
+		return $this->messageOrigine;
 	}
 }
