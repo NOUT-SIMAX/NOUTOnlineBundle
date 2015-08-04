b671bd3eb5b6a0c9a72ffcba31d413cda277c0e3
ninon <ninon@nout.fr>
2015-08-04 16:40:32 +0200

    SessionDisconnectListener

---
+++ Service/SessionDisconnectListener.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ninon
+ * Date: 04/08/2015
+ * Time: 16:02
+ */
+
+namespace NOUT\Bundle\ContextsBundle\Service;
+
+
+use Symfony\Component\EventDispatcher\GenericEvent;
+
+class SessionDisconnectListener
+{
+    /**
+     * @var NOUTClient
+     */
+    private $m_clNOUTClient;
+
+    public function __construct(NOUTClient $client)
+    {
+        $this->m_clNOUTClient = $client;
+    }
+
+    public function disconnect(GenericEvent $event)
+    {
+        //$tokenSession = $event->getSubject();
+
+        $clCache = $this->m_clNOUTClient->getCacheSession();
+        $clCache->deleteAll();
+    }
+}
