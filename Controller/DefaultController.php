32410743d11528153fcbcf5c404861366df3843a
ninon <ninon@nout.fr>
2016-11-03 14:41:14 +0100

    test

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -1,62 +1,9 @@
 <?php
 
 namespace NOUT\Bundle\NOUTOnlineBundle\Controller;
-
-use NOUT\Bundle\NOUTOnlineBundle\Entity\Header\OptionDialogue;
-use NOUT\Bundle\NOUTOnlineBundle\Entity\Parametre\CalculationListType;
-use NOUT\Bundle\NOUTOnlineBundle\Entity\Parametre\ColListType;
-use NOUT\Bundle\NOUTOnlineBundle\Entity\Parametre\ConditionColonne;
-use NOUT\Bundle\NOUTOnlineBundle\Entity\Parametre\ConditionFileNPI;
-use NOUT\Bundle\NOUTOnlineBundle\Entity\Parametre\ConditionOperateur;
-use NOUT\Bundle\NOUTOnlineBundle\Entity\Parametre\GetTokenSession;
-use NOUT\Bundle\NOUTOnlineBundle\Entity\Parametre\ReorderList;
-use NOUT\Bundle\NOUTOnlineBundle\Entity\Parametre\ReorderSubList;
-use NOUT\Bundle\NOUTOnlineBundle\Entity\Parametre\SetOrderList;
-use NOUT\Bundle\NOUTOnlineBundle\Entity\Parametre\SetOrderSubList;
-use NOUT\Bundle\NOUTOnlineBundle\Entity\Record\EnregTableauArray;
-use NOUT\Bundle\NOUTOnlineBundle\Entity\Record\Record;
-
-use NOUT\Bundle\NOUTOnlineBundle\Entity\ReponseWebService\MessageBox;
-use NOUT\Bundle\NOUTOnlineBundle\Entity\ReponseWebService\ReponseWSParser;
-use NOUT\Bundle\NOUTOnlineBundle\Entity\ReponseWebService\XMLResponseWS;
-use NOUT\Bundle\NOUTOnlineBundle\REST\OnlineServiceProxy as RESTProxy;
-use NOUT\Bundle\NOUTOnlineBundle\SOAP\OnlineServiceProxy as SOAPProxy;
-use NOUT\Bundle\NOUTOnlineBundle\SOAP\WSDLEntity\Cancel;
-use NOUT\Bundle\NOUTOnlineBundle\SOAP\WSDLEntity\ConfirmResponse;
-use NOUT\Bundle\NOUTOnlineBundle\SOAP\WSDLEntity\Create;
-use NOUT\Bundle\NOUTOnlineBundle\SOAP\WSDLEntity\CreateFrom;
-use NOUT\Bundle\NOUTOnlineBundle\SOAP\WSDLEntity\Delete;
-use NOUT\Bundle\NOUTOnlineBundle\SOAP\WSDLEntity\Display;
-use NOUT\Bundle\NOUTOnlineBundle\SOAP\WSDLEntity\DrillThrough;
-use NOUT\Bundle\NOUTOnlineBundle\SOAP\WSDLEntity\Execute;
-use NOUT\Bundle\NOUTOnlineBundle\SOAP\WSDLEntity\GetCalculation;
-use NOUT\Bundle\NOUTOnlineBundle\SOAP\WSDLEntity\GetChart;
-use NOUT\Bundle\NOUTOnlineBundle\SOAP\WSDLEntity\GetColInRecord;
-use NOUT\Bundle\NOUTOnlineBundle\SOAP\WSDLEntity\GetEndAutomatism;
-use NOUT\Bundle\NOUTOnlineBundle\SOAP\WSDLEntity\GetPlanningInfo;
-use NOUT\Bundle\NOUTOnlineBundle\SOAP\WSDLEntity\GetStartAutomatism;
-use NOUT\Bundle\NOUTOnlineBundle\SOAP\WSDLEntity\GetTableChild;
-use NOUT\Bundle\NOUTOnlineBundle\SOAP\WSDLEntity\ListParams;
-use NOUT\Bundle\NOUTOnlineBundle\SOAP\WSDLEntity\Modify;
-use NOUT\Bundle\NOUTOnlineBundle\SOAP\WSDLEntity\PrintParams;
-use NOUT\Bundle\NOUTOnlineBundle\SOAP\WSDLEntity\Request;
-use NOUT\Bundle\NOUTOnlineBundle\SOAP\WSDLEntity\RequestParam;
-use NOUT\Bundle\NOUTOnlineBundle\SOAP\WSDLEntity\Search;
-use NOUT\Bundle\NOUTOnlineBundle\SOAP\WSDLEntity\SelectForm;
-use NOUT\Bundle\NOUTOnlineBundle\SOAP\WSDLEntity\SelectItems;
-use NOUT\Bundle\NOUTOnlineBundle\SOAP\WSDLEntity\SelectPrintTemplate;
-use NOUT\Bundle\NOUTOnlineBundle\SOAP\WSDLEntity\TransformInto;
-use NOUT\Bundle\NOUTOnlineBundle\SOAP\WSDLEntity\Update;
-
-
-
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route; // this imports the annotations
-
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
-
-
-
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 
 /**
@@ -71,6 +18,37 @@ class DefaultController extends Controller
     public function indexAction()
     {
     	return $this->render('NOUTOnlineBundle:Default:index.html.twig');
+    }
+
+    /**
+     * @Route("/test", name="online_test")
+     */
+    public function testAction(Request $request)
+    {
+        ob_start();
+
+        //var_dump($var);
+        //$request = Request::createFromGlobals();
+
+        // the URI being requested (e.g. /about) minus any query parameters
+        var_dump($request->getPathInfo());
+
+
+        // retrieve $_SERVER variables
+        var_dump($request->server->get('HTTP_HOST'));
+
+        // /blog/my-blog-post
+        var_dump($this->generateUrl('index'));
+
+        var_dump($_SERVER);
+
+
+
+
+        $containt = ob_get_contents();
+        ob_get_clean();
+
+        return $this->render('NOUTOnlineBundle:Default:debug.html.twig', array('containt' => $containt));
     }
 
 }
