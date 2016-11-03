a561e7f488215ddc3987ab1caf95526500c12740
ninon <ninon@nout.fr>
2016-11-03 14:02:41 +0100

    correction gestion de cache

--- Cache/NOUTXCacheCache.php
+++ Cache/NOUTXCacheCache.php
@@ -13,7 +13,11 @@ class NOUTXCacheCache extends NOUTCacheProvider
 {
 
     /**
-     * {@inheritdoc}
+     * Fetches an entry from the cache.
+     *
+     * @param string $id The id of the cache entry to fetch.
+     *
+     * @return mixed|false The cached data or FALSE, if no cache entry exists for the given id.
      */
     protected function doFetch($id)
     {
@@ -21,7 +25,11 @@ class NOUTXCacheCache extends NOUTCacheProvider
     }
 
     /**
-     * {@inheritdoc}
+     * Tests if an entry exists in the cache.
+     *
+     * @param string $id The cache id of the entry to check for.
+     *
+     * @return bool TRUE if a cache entry exists for the given cache id, FALSE otherwise.
      */
     protected function doContains($id)
     {
@@ -29,7 +37,14 @@ class NOUTXCacheCache extends NOUTCacheProvider
     }
 
     /**
-     * {@inheritdoc}
+     * Puts data into the cache.
+     *
+     * @param string $id       The cache id.
+     * @param string $data     The cache entry/data.
+     * @param int    $lifeTime The lifetime. If != 0, sets a specific lifetime for this
+     *                           cache entry (0 => infinite lifeTime).
+     *
+     * @return bool TRUE if the entry was successfully stored in the cache, FALSE otherwise.
      */
     protected function doSave($id, $data, $lifeTime = 0)
     {
@@ -37,7 +52,11 @@ class NOUTXCacheCache extends NOUTCacheProvider
     }
 
     /**
-     * {@inheritdoc}
+     * Deletes a cache entry.
+     *
+     * @param string $id The cache id.
+     *
+     * @return bool TRUE if the cache entry was successfully deleted, FALSE otherwise.
      */
     protected function doDelete($id)
     {
@@ -45,9 +64,11 @@ class NOUTXCacheCache extends NOUTCacheProvider
     }
 
     /**
-     * {@inheritdoc}
+     * Flushes all cache entries.
+     *
+     * @return bool TRUE if the cache entries were successfully flushed, FALSE otherwise.
      */
-    protected function doFlush()
+    protected function doFlushAll()
     {
         $this->checkAuthorization();
 
