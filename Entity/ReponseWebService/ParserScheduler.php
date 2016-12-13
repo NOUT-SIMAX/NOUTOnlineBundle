05c4ef1596618f6e276a52fc6aa9038045928b4c
chris <christophe.surun@nout.fr>
2016-12-13 15:17:09 +0100

    Refactoring PHP + Ajout parser pour Scheduler

---
+++ Entity/ReponseWebService/ParserScheduler.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Chris
+ * Date: 12/12/2016
+ * Time: 11:50
+ */
+
+namespace NOUT\Bundle\NOUTOnlineBundle\Entity\ReponseWebService;
+
+/*
+ * Parser pour le NOUVEAU planning (DHX)
+ */
+
+use NOUT\Bundle\NOUTOnlineBundle\Entity\Langage;
+use NOUT\Bundle\NOUTOnlineBundle\Entity\Record\RecordList;
+use NOUT\Bundle\NOUTOnlineBundle\Entity\Record\StructureElement;
+
+class ParserScheduler extends ParserList
+{
+
+    /**
+     * @var ParserRecordList
+     */
+    protected $m_clParserScheduler;
+
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->m_clParserScheduler = new ParserRecordList();
+    }
+
+
+    /**
+     * Parse le scheduler
+     * Ne doit pas être trop volumineuse
+     * @param XMLResponseWS $clReponseXML
+     */
+    public function ParseScheduler(XMLResponseWS $clReponseXML)
+    {
+        $ndSchema    = $clReponseXML->getNodeXSDRessource();
+        $ndXML       = $clReponseXML->getNodeXMLRessource();
+        $idForm      = Langage::TABL_Ressource; //toutes les ressources sont fils du tableau ressource
+
+        if (isset($ndSchema))
+        {
+            $this->m_clParserScheduler->ParseXSD($ndSchema, StructureElement::NV_XSD_List);
+        }
+
+        $this->m_clParserScheduler->ParseXML($ndXML, $idForm, StructureElement::NV_XSD_List);
+    }
+
+    /**
+     * @param XMLResponseWS $clReponseXML
+     * @return RecordList
+     */
+    public function getScheduler(XMLResponseWS $clReponseXML)
+    {
+        $sIDForm        = Langage::TABL_Ressource;
+
+        $clStructElem = $this->m_clParserList->getStructureElem($sIDForm, StructureElement::NV_XSD_List);
+
+        // Instance d'une nouvelle clList avec toutes les données précédentes
+        $clList = new RecordList('', '', $sIDForm, $this->m_clParserScheduler->m_TabEnregTableau, $clStructElem);
+
+        // Paramètres pour la clList
+        $clList->setRecordCache($this->m_clParserScheduler->getFullCache());
+        return $clList;
+    }
+}
