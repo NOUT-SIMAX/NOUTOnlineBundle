3d26653026b5357e4e1ed6433e8edd8b2c7f848e
Simon Boeuf <simon@nout.fr>
2017-03-09 11:31:28 +0100

    added SelecChoice WSDLEntity

---
+++ SOAP/WSDLEntity/SelectChoice.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: simon
+ * Date: 08/03/2017
+ * Time: 12:54
+ */
+
+namespace NOUT\Bundle\NOUTOnlineBundle\SOAP\WSDLEntity;
+
+//-------------------------------------------------------------------------------------------------------------------
+// Ensemble de classes utilisé par la classe SimaxOnlineServiceProxy
+// Note : les conventions de code peuvent semblé non respecté sur les nom de variables, mais elle corresponde en realite
+// au fichier WSDL
+//-------------------------------------------------------------------------------------------------------------------
+
+
+class SelectChoice
+{
+    public $Choice; // string
+}
