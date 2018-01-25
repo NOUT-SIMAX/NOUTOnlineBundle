bbbb466d86b91d7cd0149b3b33141b0e0ead3a9d
Logar <simon.boeuf@gmail.com>
2018-01-25 15:15:59 +0100

    added containercol option

--- Entity/Record/StructureColonne.php
+++ Entity/Record/StructureColonne.php
@@ -564,6 +564,7 @@ abstract class StructureColonne
 	const OPTION_LinkControl = 'linkControl';    // pour les colonnes (controles de validité)
 
 	const OPTION_Hidden   = "hidden"; // Namespace déjà géré
+	const OPTION_ContainerCol   = "containerCol"; // Namespace déjà géré
 	const OPTION_ReadOnly = "readOnly";
 	const OPTION_Disabled = "disabled";
 
