9e80ab26e391e88fb6eeaf7fc0469fbdefb3fcdf
ninon lafourcade <ninon@nout.fr>
2020-05-19 18:20:19 +0200

    deplacement en prévision merge pour upgrade symfony

--- Controller/DefaultController.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace NOUT\Bundle\ContextsBundle\Controller;
-
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
-class DefaultController extends Controller
-{
-}
--- NOUTContextsBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace NOUT\Bundle\ContextsBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class NOUTContextsBundle extends Bundle
-{
-}
