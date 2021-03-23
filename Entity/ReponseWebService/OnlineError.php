fe787a503e80ca71a6abac1b254e15c19667add3
ninon lafourcade <ninon@nout.fr>
2021-03-23 15:54:13 +0100

    erreur pasdroitaction

--- Entity/ReponseWebService/OnlineError.php
+++ Entity/ReponseWebService/OnlineError.php
@@ -140,6 +140,10 @@ class OnlineError implements \JsonSerializable
 	const ERR_ACTION_NOCTXT  = 1001;
 
 	//constante pour les codes d'erreurs
+
+    const ERR_MAX_ANALYSEMEASSAGE_PASDROITACTION = 403;
+
+
 	const ERR_WS_AUTRE                         = 1000;
 	const ERR_WS_LANGOBJECT_GETVALCOL          = 1001;
 	const ERR_WS_LANGOBJECT_NULL               = 1002;
