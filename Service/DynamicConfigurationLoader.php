1059cdf087959f106230151952c5c6eaa4e7967c
ninon lafourcade <ninon@nout.fr>
2022-10-06 09:41:18 +0200

    redirection pour multitenant si site inconnu

--- Service/DynamicConfigurationLoader.php
+++ Service/DynamicConfigurationLoader.php
@@ -3,7 +3,6 @@
 
 namespace NOUT\Bundle\NOUTOnlineBundle\Service;
 
-
 use NOUT\Bundle\NOUTOnlineBundle\DependencyInjection\Configuration;
 use Symfony\Component\Config\Definition\Processor;
 use Symfony\Component\Yaml\Yaml;
@@ -11,20 +10,30 @@ use Symfony\Component\Yaml\Yaml;
 class DynamicConfigurationLoader
 {
 
-    /** @var array  */
-    protected $_config;
+    /** @var array|null  */
+    protected $_config = null;
+
+    /** @var bool  */
+    protected $_multitenantNotFound = false;
+
+    /** @var string  */
+    protected $_hostname = '';
 
     /**
      * ConfigurationLoader constructor.
+     * @param array  $aMultitenant
      * @param string $sConfigDir
-     * @param array  $config
+     * @param array  $defaultConfig
+     * @param string $sHostname
      */
-    public function __construct(bool $bMultitenant, string $sConfigDir, array $defaultConfig, string $sHostname)
+    public function __construct(array $aMultitenant, string $sConfigDir, array $defaultConfig, string $sHostname)
     {
-        if (!$bMultitenant){
+        if (!$aMultitenant['actif']){
             $this->_config = $defaultConfig;
         }
         else {
+            $this->_hostname = $sHostname;
+
             //il faut faire en fonction du hostname
             if (($sHostname=='localhost') && array_key_exists('SERVER_NAME', $_SERVER)){
                 $sHostname=str_replace('www.', '', $_SERVER['SERVER_NAME']);
@@ -44,15 +53,39 @@ class DynamicConfigurationLoader
                 $this->_config = $processor->process($tree_builder->buildTree(), $config_readed);
             }
             else {
-                $this->_config = $defaultConfig;
+                $this->_multitenantNotFound = true;
             }
 
         }
     }
 
-    public function getParameter($name)
+    /**
+     * @param string $name
+     * @param null   $default
+     * @return mixed
+     */
+    public function getParameter(string $name, $default=null)
     {
+        if (is_null($this->_config) || !isset($this->_config[$name])){
+            return $default;
+        }
         return $this->_config[$name];
+    }
+
+    /**
+     * @return bool
+     */
+    public function isMultitenantNotFound(): bool
+    {
+        return $this->_multitenantNotFound;
+    }
+
+    /**
+     * @return string
+     */
+    public function getHostname(): string
+    {
+        return $this->_hostname;
     }
 
 }
