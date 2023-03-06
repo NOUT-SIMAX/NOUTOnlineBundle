9286da4917699ea9512b85eee436bac790af6abe
ninon lafourcade <ninon@nout.fr>
2023-03-06 10:03:05 +0100

    renommage pour les modeles JSON webix gestion des info des boutons

--- Entity/ActionResult.php
+++ Entity/ActionResult.php
@@ -12,10 +12,10 @@ use NOUT\Bundle\NOUTOnlineBundle\Entity\ReponseWebService\ConnectedUser;
 use NOUT\Bundle\NOUTOnlineBundle\Entity\ReponseWebService\Count;
 use NOUT\Bundle\NOUTOnlineBundle\Entity\ReponseWebService\CurrentAction;
 use NOUT\Bundle\NOUTOnlineBundle\Entity\ReponseWebService\CurrentRequest;
+use NOUT\Bundle\NOUTOnlineBundle\Entity\ReponseWebService\Element;
 use NOUT\Bundle\NOUTOnlineBundle\Entity\ReponseWebService\FolderCount;
 use NOUT\Bundle\NOUTOnlineBundle\Entity\ReponseWebService\ValidateError;
 use NOUT\Bundle\NOUTOnlineBundle\Entity\ReponseWebService\XMLResponseWS;
-use NOUT\Bundle\NOUTOnlineBundle\Entity\ReponseWebService\Element;
 
 class ActionResult
 {
@@ -370,7 +370,7 @@ class ActionResult
      */
     public function getTypeAction(): string
     {
-        return $this->getAction()->getTypeAction();
+        return $this->getAction()->getIDTypeAction();
     }
 
 
