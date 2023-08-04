e679e95e812108783ab542962d0467bad259110f
ninon lafourcade <ninon@simax.fr>
2023-08-04 11:12:07 +0200

    depreciation et sonarlint

--- DataCollector/NOUTOnlineDataCollector.php
+++ DataCollector/NOUTOnlineDataCollector.php
@@ -17,31 +17,31 @@ use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInt
 
 class NOUTOnlineDataCollector  extends DataCollector
 {
-	private $m_clLogger;
-	private $m_clTokenStorage;
+    private $clLogger;
+    private $clTokenStorage;
 
-	public function getName()
-	{
-		return 'NOUTOnline';
-	}
+    public function getName()
+    {
+        return 'NOUTOnline';
+    }
 
-	public function __construct(NOUTOnlineLogger $clLogger, TokenStorageInterface $tokenStorage = null)
-	{
-		$this->m_clLogger          = $clLogger;
-		$this->m_clTokenStorage    = $tokenStorage;
-	}
+    public function __construct(NOUTOnlineLogger $clLogger, TokenStorageInterface $tokenStorage = null)
+    {
+        $this->clLogger       = $clLogger;
+        $this->clTokenStorage = $tokenStorage;
+    }
 
     public function reset()
     {
 
     }
-	public function collect(Request $request, Response $response, \Throwable $exception = null)
-	{
-		$queries = $this->m_clLogger->m_TabQueries;
+    public function collect(Request $request, Response $response, \Throwable $exception = null)
+    {
+        $queries = $this->clLogger->m_TabQueries;
 
-		$this->data = array(
-			'queries'     => $queries,
-		);
+        $this->data = array(
+            'queries'     => $queries,
+        );
 
 
         $this->data['authenticated'] = false;
@@ -49,68 +49,69 @@ class NOUTOnlineDataCollector  extends DataCollector
         $this->data['time_zone']     = '';
         $this->data['user']          = '';
         $this->data['superviseur']   = false;
-        $this->data['ip']			 = '';
-        $this->data['version']		 = '';
+        $this->data['ip']            = '';
+        $this->data['version']       = '';
         $this->data['extranet']      = false;
 
-		if (!is_null($this->m_clTokenStorage) && !is_null($token = $this->m_clTokenStorage->getToken()))
-		{
-			$this->data['authenticated'] = $token->isAuthenticated();
-            $this->data['user']          = $token->getUsername();
+        if ( !is_null($this->clTokenStorage) && !is_null($token = $this->clTokenStorage->getToken()))
+        {
+            $username = $token->getUsername();
+            $this->data['user']          = $username;
+            $this->data['authenticated'] = !is_null($username);
 
-			$tabRole = $token->getRoleNames();
-			$this->data['superviseur'] = in_array('ROLE_SUPERVISEUR', $tabRole);
+            $tabRole = $token->getRoleNames();
+            $this->data['superviseur'] = in_array('ROLE_SUPERVISEUR', $tabRole);
 
-			if ($token instanceof NOUTToken)
-			{
-			    $oExtranetUsernameToken = $token->getExtranetUsernameToken();
+            if ($token instanceof NOUTToken)
+            {
+                $oExtranetUsernameToken = $token->getExtranetUsernameToken();
 
                 $this->data['extranet']      = $oExtranetUsernameToken ? $oExtranetUsernameToken->Username : '';
-				$this->data['session_token'] = $token->getSessionToken();
-				$this->data['time_zone']     = $token->getTimeZone();
-				$this->data['ip']            = $token->getIP();
+                $this->data['session_token'] = $token->getSessionToken();
+                $this->data['time_zone']     = $token->getTimeZone();
+                $this->data['ip']            = $token->getIP();
                 $this->data['version']       = $token->getVersionNO();
-			}
-		}
-	}
+            }
+        }
+    }
 
-	/**
-	 * @return int
-	 */
-	public function getQueryCount()
-	{
-		return count($this->data['queries']);
-	}
+    /**
+     * @return int
+     */
+    public function getQueryCount()
+    {
+        return count($this->data['queries']);
+    }
 
-	/**
-	 * @return array
-	 */
-	public function getQueries()
-	{
-		return $this->data['queries'];
-	}
+    /**
+     * @return array
+     */
+    public function getQueries()
+    {
+        return $this->data['queries'];
+    }
 
-	/**
-	 * @return int
-	 */
-	public function getTime()
-	{
-		$time = 0;
-		foreach ($this->data['queries'] as $query)
-		{
-			$time += $query['executionMS'];
-		}
+    /**
+     * @return int
+     */
+    public function getTime()
+    {
+        $time = 0;
+        foreach ($this->data['queries'] as $query)
+        {
+            $time += $query['executionMS'];
+        }
 
-		return $time;
-	}
+        return $time;
+    }
 
-	/**
-	 * @return bool
-	 */
-	public function getSuperviseur()
-	{
-		return $this->data['superviseur'];
-	}
+    /**
+     * @return bool
+     */
+    public function getSuperviseur()
+    {
+        return $this->data['superviseur'];
+    }
 
     /**
      * @return bool
@@ -132,51 +133,51 @@ class NOUTOnlineDataCollector  extends DataCollector
     }
 
 
-	/**
-	 * Checks if the user is authenticated or not.
-	 *
-	 * @return bool    true if the user is authenticated, false otherwise
-	 */
-	public function isAuthenticated()
-	{
-		return $this->data['authenticated'];
-	}
+    /**
+     * Checks if the user is authenticated or not.
+     *
+     * @return bool    true if the user is authenticated, false otherwise
+     */
+    public function isAuthenticated()
+    {
+        return $this->data['authenticated'];
+    }
 
-	/**
-	 * Gets the user.
-	 *
-	 * @return string The user
-	 */
-	public function getUser()
-	{
-		return $this->data['user'];
-	}
+    /**
+     * Gets the user.
+     *
+     * @return string The user
+     */
+    public function getUser()
+    {
+        return $this->data['user'];
+    }
 
-	/**
-	 * Gets the client IP.
-	 *
-	 * @return string the client IP
-	 */
-	public function getIP()
-	{
-		return $this->data['ip'];
-	}
-	/**
-	 * get the token of the session
-	 *
-	 * @return string
-	 */
-	public function getSessionToken()
-	{
-		return $this->data['session_token'];
-	}
+    /**
+     * Gets the client IP.
+     *
+     * @return string the client IP
+     */
+    public function getIP()
+    {
+        return $this->data['ip'];
+    }
+    /**
+     * get the token of the session
+     *
+     * @return string
+     */
+    public function getSessionToken()
+    {
+        return $this->data['session_token'];
+    }
 
-	/**
-	 * get the time zone
-	 * @return string
-	 */
-	public function getTimeZone()
-	{
-		return $this->data['time_zone'];
-	}
+    /**
+     * get the time zone
+     * @return string
+     */
+    public function getTimeZone()
+    {
+        return $this->data['time_zone'];
+    }
 }
